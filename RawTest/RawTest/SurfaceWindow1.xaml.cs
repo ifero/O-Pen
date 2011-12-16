2113cf0f6fec199666a20696d1f05a872bcda012
Andrea Pacino <andrea.pacino87@gmail.com>
2011-12-16 14:31:53 +0100

    adding files

---
+++ RawTest/RawTest/SurfaceWindow1.xaml.cs
@@ -0,0 +1,220 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Runtime.InteropServices;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Microsoft.Surface;
+using Microsoft.Surface.Presentation;
+using Microsoft.Surface.Presentation.Controls;
+using Microsoft.Surface.Core;
+
+
+namespace RawTest
+{
+    /// <summary>
+    /// Interaction logic for SurfaceWindow1.xaml
+    /// </summary>
+    public partial class SurfaceWindow1 : SurfaceWindow
+    {
+        private ContactTarget contactTarget;
+        private IntPtr hwnd;
+        private byte[] normalizedImage;
+        private ImageMetrics imageMetrics;
+        private bool imageAvailable;
+        private ColorPalette pal;
+        private Bitmap frame;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public SurfaceWindow1()
+        {
+            InitializeComponent();
+            InitializeSurfaceInput();
+            // Add handlers for Application activation events
+            AddActivationHandlers();
+        }
+
+
+        private void InitializeSurfaceInput()
+        {
+            hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            contactTarget = new Microsoft.Surface.Core.ContactTarget(hwnd);
+            contactTarget.EnableInput();
+
+            EnableRawImage();
+        }
+
+        /// <summary>
+        /// Occurs when the window is about to close. 
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            // Remove handlers for Application activation events
+            RemoveActivationHandlers();
+        }
+
+        /// <summary>
+        /// Adds handlers for Application activation events.
+        /// </summary>
+        private void AddActivationHandlers()
+        {
+            // Subscribe to surface application activation events
+            ApplicationLauncher.ApplicationActivated += OnApplicationActivated;
+            ApplicationLauncher.ApplicationPreviewed += OnApplicationPreviewed;
+            ApplicationLauncher.ApplicationDeactivated += OnApplicationDeactivated;
+        }
+
+        /// <summary>
+        /// Removes handlers for Application activation events.
+        /// </summary>
+        private void RemoveActivationHandlers()
+        {
+            // Unsubscribe from surface application activation events
+            ApplicationLauncher.ApplicationActivated -= OnApplicationActivated;
+            ApplicationLauncher.ApplicationPreviewed -= OnApplicationPreviewed;
+            ApplicationLauncher.ApplicationDeactivated -= OnApplicationDeactivated;
+        }
+
+        /// <summary>
+        /// This is called when application has been activated.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnApplicationActivated(object sender, EventArgs e)
+        {
+            //TODO: enable audio, animations here
+        }
+
+        /// <summary>
+        /// This is called when application is in preview mode.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnApplicationPreviewed(object sender, EventArgs e)
+        {
+            //TODO: Disable audio here if it is enabled
+
+            //TODO: optionally enable animations here
+        }
+
+        /// <summary>
+        ///  This is called when application has been deactivated.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnApplicationDeactivated(object sender, EventArgs e)
+        {
+            //TODO: disable audio, animations here
+        }
+
+        private void EnableRawImage()
+        {
+            contactTarget.EnableImage(ImageType.Normalized);
+            contactTarget.FrameReceived += new EventHandler<FrameReceivedEventArgs>(target_FrameReceived);
+        }
+
+        private void DisableRawImage()
+        {
+            contactTarget.DisableImage(ImageType.Normalized);
+            contactTarget.FrameReceived -= new EventHandler<FrameReceivedEventArgs>(target_FrameReceived);
+        }
+
+        void target_FrameReceived(object sender, FrameReceivedEventArgs e)
+        {
+            imageAvailable = false;
+            int paddingLeft, paddingRight;
+            if (normalizedImage == null)
+            {
+                imageAvailable = e.TryGetRawImage(ImageType.Normalized,
+                  Microsoft.Surface.Core.InteractiveSurface.DefaultInteractiveSurface.Left,
+                  Microsoft.Surface.Core.InteractiveSurface.DefaultInteractiveSurface.Top,
+                  Microsoft.Surface.Core.InteractiveSurface.DefaultInteractiveSurface.Width,
+                  Microsoft.Surface.Core.InteractiveSurface.DefaultInteractiveSurface.Height,
+                  out normalizedImage, out imageMetrics, out paddingLeft, out paddingRight);
+            }
+            else
+            {
+                imageAvailable = e.UpdateRawImage(ImageType.Normalized, normalizedImage,
+                  Microsoft.Surface.Core.InteractiveSurface.DefaultInteractiveSurface.Left,
+                  Microsoft.Surface.Core.InteractiveSurface.DefaultInteractiveSurface.Top,
+                  Microsoft.Surface.Core.InteractiveSurface.DefaultInteractiveSurface.Width,
+                  Microsoft.Surface.Core.InteractiveSurface.DefaultInteractiveSurface.Height);
+            }
+
+            if (!imageAvailable)
+                return;
+
+            DisableRawImage();
+        
+            ShowInWPF();
+
+            imageAvailable = false;
+            EnableRawImage();
+        }
+
+        /// <summary>
+        /// Show the Raw Image of the Surface's Camera on Surface Screen
+        /// </summary>
+        private void ShowInWPF()
+        {
+            GCHandle h = GCHandle.Alloc(normalizedImage, GCHandleType.Pinned);
+            IntPtr ptr = h.AddrOfPinnedObject();
+            frame = new Bitmap(imageMetrics.Width,
+                                  imageMetrics.Height,
+                                  imageMetrics.Stride,
+                                  System.Drawing.Imaging.PixelFormat.Format8bppIndexed,
+                                  ptr);
+            Convert8bppBMPToGrayscale(frame);
+            iCapturedFrame.Source = Bitmap2BitmapImage(frame);
+        }
+
+        /// <summary>
+        /// Convert RGB Bitmap to a GrayScale Bitmap
+        /// </summary>
+        /// <param name="bmp"></param>
+        private void Convert8bppBMPToGrayscale(Bitmap bmp)
+        {
+            if (pal == null) // pal is defined at module level as --- ColorPalette pal;
+            {
+                pal = bmp.Palette;
+                for (int i = 0; i < 256; i++)
+                {
+                    pal.Entries[i] = System.Drawing.Color.FromArgb(i, i, i);
+                }
+            }
+            bmp.Palette = pal;
+        }
+
+        /// <summary>
+        /// Convert from Bitmap to BitmapImage
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <returns></returns>
+        private BitmapImage Bitmap2BitmapImage(Bitmap bitmap)
+        {
+            MemoryStream ms = new MemoryStream();
+            bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage bImg = new System.Windows.Media.Imaging.BitmapImage();
+            bImg.BeginInit();
+            bImg.StreamSource = new MemoryStream(ms.ToArray());
+            bImg.EndInit();
+            return bImg;
+        }
+    }
+}
