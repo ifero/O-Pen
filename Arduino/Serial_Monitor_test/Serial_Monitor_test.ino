0e73664fb2eab39ab7ea71ed8ff36bd43b0f7c35
Andrea Pacino <andrea.pacino87@gmail.com>
2012-02-20 11:35:50 +0100

    last commit

--- Arduino/Serial_Monitor_test/Serial_Monitor_test.ino
+++ Arduino/Serial_Monitor_test/Serial_Monitor_test.ino
@@ -1,13 +1,13 @@
 int i = 1;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(19200);
 }
 
 void loop() {
   Serial.print("just saying hi for the ");
   Serial.print(i);
   Serial.print("th time. \n");
-  delay(2000);
+  delay(200);
   i++;
 }
